91f032c4495063acad0a2711e4205ddc862df784
prettynatty <fear@loathing.in>
2016-01-24 06:00:02 +0300

    fix(server): . -> /

--- push-server/src/main/scala/im/actor/push/model/Subscription.scala
+++ push-server/src/main/scala/im/actor/push/model/Subscription.scala
@@ -4,7 +4,7 @@ import java.net.URLEncoder
 import java.util.UUID
 
 final case class Subscription(appId: Int, id: String) {
-  def topic = s"actor.$appId.subscription.$id"
+  def topic = s"actor/$appId/subscription/$id"
 
   def endpoint(baseUri: String) = s"$baseUri/apps/$appId/subscriptions/${URLEncoder.encode(id, "UTF-8")}"
 }
